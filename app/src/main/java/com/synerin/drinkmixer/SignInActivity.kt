32c097516b676fae6e75f1ff4559045701a835b3
Synerin <36575784+Synerin@users.noreply.github.com>
2020-12-13 19:55:52 -0500

    build basic activity

--- app/src/main/java/com/synerin/drinkmixer/SignInActivity.kt
+++ app/src/main/java/com/synerin/drinkmixer/SignInActivity.kt
@@ -1,2 +1,23 @@
 package com.synerin.drinkmixer
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "SignInActivity"
+
+class SignInActivity : AppCompatActivity() {
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+    }
+}
